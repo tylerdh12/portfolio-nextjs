17ba8045e6ea1a653f93bd9d9ac3d19fb2f29c92
Tyler Harper <tylerdh12@gmail.com>
2020-10-07 08:36:22 -0700

    seperate styles from component

--- pages/portfolio/[id].tsx
+++ pages/portfolio/[id].tsx
@@ -7,22 +7,10 @@ const Project = ({ project }: any) => {
   return (
     <Layout title="Portfolio">
       <div style={{ display: "flex" }}>
-        <h1 style={{ textAlign: "center", width: "100%", marginTop: 40 }}>
-          {project.project_name}
-        </h1>
+        <h1 className="project-name">{project.project_name}</h1>
       </div>
       <div style={{ textAlign: "center" }}>
-        <p
-          style={{
-            textAlign: "center",
-            width: "90%",
-            marginLeft: "auto",
-            marginRight: "auto",
-            marginBottom: 40,
-          }}
-        >
-          {project.description}
-        </p>
+        <p className="project-description">{project.description}</p>
         <div className="button-container">
           <Link href={project.github_link} passHref>
             <a target="_blank" className="link-button">
@@ -35,30 +23,15 @@ const Project = ({ project }: any) => {
             </a>
           </Link>
         </div>
-        <h3 style={{ textAlign: "center" }}>Technologies</h3>
-        <ul
-          style={{
-            textAlign: "center",
-            padding: 0,
-            listStyle: "none",
-            display: "flex",
-            flexBasis: 10,
-            flexDirection: "column",
-            marginBottom: 40,
-          }}
-        >
+        <h3 className="technologies-title">Technologies</h3>
+        <ul className="technology-ul">
           {project.technologies.map((item: React.ReactNode) => {
-            return <li style={{ padding: 0.8 }}>{item}</li>;
+            return <li className="technology-item">{item}</li>;
           })}
         </ul>
-        <div style={{ textAlign: "center" }}>
+        <div className="portfolio-img-container">
           {project.image_urls.map((item: any) => {
-            return (
-              <img
-                src={item}
-                className="portfolio-img"
-              />
-            );
+            return <img src={item} className="portfolio-img" />;
           })}
         </div>
       </div>
