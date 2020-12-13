f3bf5b10e1591026910d98e1507efafab3b7b00a
Tyler Harper <tylerdh12@gmail.com>
2020-12-13 09:01:59 -0800

    setup google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,20 @@
-import { motion } from "framer-motion";
-import React from "react";
-import "../styles/styles.scss";
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
-export default function MyApp({ router, Component, pageProps }) {
-  return (
-    <motion.div
-      key={router.route}
-      initial="pageInitial"
-      animate="pageAnimate"
-      variants={{
-        pageInitial: {
-          opacity: 0,
-        },
-        pageAnimate: {
-          opacity: 1,
-        },
-      }}
-      transition={{ duration: 1 }}
-    >
-      <Component {...pageProps} />
-    </motion.div>
-  );
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return <Component {...pageProps} />
 }
+
+export default App
